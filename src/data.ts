3f81881019453feb98cc630cd6b5cebe278afa34
Michał <michal.jedro@wp.pl>
2023-01-16 21:20:07 +0100

    zmiana dla brancha

--- src/data.ts
+++ src/data.ts
@@ -15,21 +15,21 @@ export const PeopleData = [
   },
   {
     id: 3,
-    name: "Larry Little",
+    name: "Irek",
     age: 36,
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
   },
   {
     id: 4,
-    name: "Sean Walsh",
+    name: "Adam Marynarz",
     age: 34,
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
   },
   {
     id: 5,
-    name: "Lola Gardner",
+    name: "Natalia Piorun",
     age: 29,
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
